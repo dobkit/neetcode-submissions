a39960cc9f14526c1b1a5dc1f58c3c08c1106a7d
dobkit <97825530+dobkit@users.noreply.github.com>
2026-07-19 22:56:32 +0700

    Add: find-target-in-rotated-sorted-array - submission-11

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-11.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // two binary search
+        // find pivot, then find target
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int midValue = nums[mid];
+            int rightValue = nums[right];
+
+            if (midValue <= rightValue) right = mid;
+            else left = mid + 1;
+        }
+        int pivot = left;
+        int pivotValue = nums[pivot];
+
+        right = n - 1;
+        left = 0;
+
+        if (target == pivotValue) return pivot;
+        else if (target > nums[right]) right = pivot - 1;
+        else left = pivot;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = nums[mid];
+
+            if (midValue == target) return mid;
+            else if (midValue < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
